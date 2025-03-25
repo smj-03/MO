b648b591c941ab5a76e302569289eb90775168ca
Szymon Chwastek <szymon.chwastek@kolomolo.com>
2025-03-25 11:17:55 +0000

    Init Commit

---
+++ Lab1.cpp
@@ -0,0 +1,64 @@
+﻿/*
+* Napisz program w języku „C/C++”, umożliwiający „doświadczalne” wyznaczenie liczby bitów
+* mantysy oraz tzw. epsylona maszynowego, tj. najmniejszej liczby E takiej, że fl(E + 1) > 1. Wyznacz
+* epsylon maszynowy dla zmiennych typu float i double i sprawdź czy da się go wyznaczyć dla
+* zmiennych typu long double. Sprawdź też ile dokładnych cyfr znaczących posiada epsylon maszynowy.
+* Aby znaleźć odpowiedź na pytanie jak napisać taki program, zacznij od wyjaśnienia kwestii jaki jest
+* związek E z precyzją arytmetyki.
+*/
+
+// Epsylon maszynowy - Rożnica pomiędzy 1, a następną dostępną liczbą.
+// E = x+ - x-, Zakładając, że x- = 1
+// E = 2 * v = 2 * 2^[-1 * (t+1)] = 2^(-t)
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    // FLOAT
+    int t_float = 0;
+    float E_float = 1.0f;
+    float temp_float;
+
+    while (1.0f + E_float > 1.0f) {
+        t_float++;
+        temp_float = E_float;
+        E_float /= 2.0f;
+    }
+
+    std::cout << "Liczba bitow mantysy dla float: " << t_float - 1 << std::endl;
+    std::cout << "Epsylon maszynowy dla float: " << std::fixed << std::setprecision(t_float) << temp_float << std::endl
+            << std::endl;
+
+    // DOUBLE
+    int t_double = 0;
+    double E_double = 1.0;
+    double temp_double;
+
+    while (1.0 + E_double > 1.0) {
+        t_double++;
+        temp_double = E_double;
+        E_double /= 2.0;
+    }
+
+    std::cout << "Liczba bitow mantysy dla double: " << t_double - 1 << std::endl;
+    std::cout << "Epsylon maszynowy dla double: " << std::fixed << std::setprecision(t_double) << temp_double <<
+            std::endl << std::endl;
+
+    // LONG DOUBLE
+    int t_long_double = 0;
+    long double E_long_double = 1.0L;
+    long double temp_long_double;
+
+    while (1.0L + E_long_double > 1.0L) {
+        t_long_double++;
+        temp_long_double = E_long_double;
+        E_long_double /= 2.0L;
+    }
+
+    std::cout << "Liczba bitow mantysy dla long double: " << t_long_double - 1 << std::endl;
+    std::cout << "Epsylon maszynowy dla long double: " << std::fixed << std::setprecision(t_long_double) <<
+            temp_long_double << std::endl;
+
+    return 0;
+}
---
+++ Lab2.cpp
@@ -0,0 +1,23 @@
+﻿/*
+* Zaimplementuj w języku „C/C++” algorytm obliczający przybliżone wartości funkcji
+* f(x) = x^3/{6[sinh(x)-x]} dla x ∈ [10^-10, 10^3], korzystając z funkcji standardowej sinh().
+* W oparciu o zbiór dokładnych wartości tej funkcji, udostępniony przez prowadzącego zajęcia,
+* zbadaj jak zmieniają się błędy względne przybliżenia funkcji w tym algorytmie, w zależności od x.
+* W tym celu wykonaj rysunek przedstawiający zależność logarytmu dziesiętnego z bezwzględnej wartości
+* błędu względnego od logarytmu dziesiętnego z x. Z wykresu odczytaj zakres zmiennej x, w którym błąd
+* względny pozostaje na poziomie błędu reprezentacji, oraz zakresy zmiennej x, w których błąd
+* względny jest większy. Wyjaśnij przyczyny obserwowanych zmian błędów. Na tej podstawie
+* zaproponuj alternatywny sposób obliczania wartości funkcji f(x) w sytuacjach gdy obserwowany
+* błąd jest duży. Dokonaj stosownej modyfikacji programu, tak aby uzyskać błąd względny na
+* poziomie błędu reprezentacji (czyli tzw. dokładność maszynową) dla dowolnego x ∈ [10^-10, 10^3].
+* Jaki typ zmiennych należy zastosować i dlaczego?
+* Do wykonania rysunku w tym ćwiczeniu (a także w niektórych dalszych ćwiczeniach) najlepiej użyć
+* programu GNUPLOT (dostępnego za darmo z Internetu).
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
