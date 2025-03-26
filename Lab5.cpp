625c20f80369cafdbc539eec67e8e15fea606350
Szymon Chwastek <szymon.chwastek@kolomolo.com>
2025-03-26 11:45:25 +0100

    feat: lab5 and lab6 content

---
+++ Lab5.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Dana jest macierz
+*     [ 5    4    3    2    1 ]                 [ 37 ]
+*     [ 10   8    7    6    5 ]                 [ 99 ]
+* A = [-1    2   -3    4   -5 ] oraz wektor b = [ -9 ]
+*     [ 6    5   -4    3   -2 ]                 [ 12 ]
+*     [ 1    2    3    4    5 ]                 [ 53 ]
+*
+* Napisz program w języku "C/C++", realizujący dekompozycję LU macierzy A, przy zastosowaniu
+* eliminacji Gaussa z częściowym wyborem elementu podstawowego, a następnie rozwiązujący układ
+* równań Ax = b.
+*
+* Uwaga: należy zaimplementować wariant dekompozycji omawiany na wykładzie.
+*
+* Program należy zrealizować w postaci dwóch odrębnych procedur: jednej, która operuje wyłącznie
+* na macierzy A, i drugiej, która operuje wyłącznie na wektorze b, korzystając z wyników działania
+* procedury pierwszej.
+*/
+
+// Nie używać A[i][j], zastosować pomocniczy wektor index[i]=i
+// A[index[i][j], b[index[i]][j]
+
+// Created by szymon.chwastek on 3/26/2025.
+// 5.2 Metoda Wyznaczników
+// 6 3 tablice przechowujace przekatne
---
+++ Lab6.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* Napisz program w języku „C/C++”, realizujący algorytm Thomasa dla macierzy trój-diagonalnej o
+* dowolnych rozmiarach N x N, a następnie zastosuj ten program do rozwiązania układu równań
+* Ax = b, w którym
+*
+*     [ 100 -1                ]                 [ 199 ]
+*     [  2  200  -3           ]                 [ 195 ]
+* A = [      4   300   5      ] oraz wektor b = [ 929 ]
+*     [          -6   200  -7 ]                 [ 954 ]
+*     [               -8  100 ]                 [ 360 ]
+*
+* Program należy zrealizować w postaci dwóch odrębnych procedur: jednej, która operuje wyłącznie
+* na macierzy A, i drugiej, która operuje wyłącznie na wektorze b, korzystając z wyników działania
+* procedury pierwszej.
+* Uwaga: ponieważ macierz trój-diagonalna jest macierzą rzadką, więc w programie
+* NIE NALEŻY używać tablic kwadratowych do reprezentacji macierzy A.
+*/
