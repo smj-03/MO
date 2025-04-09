beec6130ae647467a5ba8fb4794470740a535e9d
Szymon Chwastek <szymon.chwastek@kolomolo.com>
2025-04-09 12:29:05 +0200

    feat: utility functions for lab7

--- Lab7.cpp
+++ Lab7.cpp
@@ -18,7 +18,98 @@
 
 #include <iostream>
 
+#define N 5
+
+#define N_MAX 100
+#define TOLX 1e-12
+#define TOLF 1e-12
+
+void jacobi(double [N][N], double [N][N], double [N][N], double [N], double [N]);
+
+void ldu_decomposition(double [N][N], double [N][N], double [N][N], double [N][N]);
+
+bool invert_matrix(double [5][5], double [5][5]);
+
+void add_matrices(double [N][N], double [N][N], double [N][N]);
+
+void print_matrix(double [N][N]);
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    double M[N][N] = {
+        {50.0, 5.0, 4.0, 3.0, 2.0},
+        {1.0, 40.0, 1.0, 2.0, 3.0},
+        {4.0, 5.0, 30.0, -5.0, -4.0},
+        {-3.0, -2.0, -1.0, 20.0, 0.0},
+        {1.0, 2.0, 3.0, 4.0, 30.0}
+    };
+
+    double b[N] = {140.0, 67.0, 62.0, 89.0, 153.0};
+
+    double L[N][N], D[N][N], U[N][N];
+
+    ldu_decomposition(M, L, D, U);
+
+    std::cout << "L MATRIX:" << std::endl;
+    print_matrix(L);
+    std::cout << std::endl;
+
+    std::cout << "D MATRIX:" << std::endl;
+    print_matrix(D);
+    std::cout << std::endl;
+
+    std::cout << "U MATRIX:" << std::endl;
+    print_matrix(U);
+    std::cout << std::endl;
+
     return 0;
+}
+
+void jacobi(double M[N][N], double b[N], double x[N]) {
+    double L[N][N], D[N][N], U[N][N];
+    ldu_decomposition(M, L, D, U);
+
+    double inv_D[N][N];
+    invert_matrix(D, inv_D);
+
+    for (int i = 0; i <)
+
+        // for (int i = 1; i < N_MAX; i++) {
+        //
+        // }
+
+
+
+}
+
+void ldu_decomposition(double M[N][N], double L[N][N], double D[N][N], double U[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (j < i) L[i][j] = M[i][j];
+            else L[i][j] = 0.0;
+
+            if (i == j) D[i][j] = M[i][j];
+            else D[i][j] = 0.0;
+
+            if (j > i) U[i][j] = M[i][j];
+            else U[i][j] = 0.0;
+        }
+    }
+}
+
+bool invert_matrix(double M[5][5], double inv_M[5][5]) {
+}
+
+void add_matrices(double A[N][N], double B[N][N], double R[N][N]) {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            R[i][j] = A[i][j] + B[i][j];
+}
+
+
+void print_matrix(double M[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("%5.2f ", M[i][j]);
+        std::cout << std::endl;
+    }
 }
