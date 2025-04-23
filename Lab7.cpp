d95a6b4d7d97aceed41fe641273e9e138e93ca0d
Szymon Chwastek <szymon.chwastek@kolomolo.com>
2025-04-23 10:00:43 +0200

    feat: lab7 finished

--- Lab7.cpp
+++ Lab7.cpp
@@ -22,8 +22,8 @@
 #define N 5
 
 #define N_MAX 100
-#define TOLX 1e-12
-#define TOLF 1e-12
+#define TOLX 1e-8
+#define TOLF 1e-8
 
 void jacobi(double [N][N], double [N], double [N]);
 
@@ -31,13 +31,11 @@ void gauss_seidel(double [N][N], double [N], double [N]);
 
 void sor(double [N][N], double [N], double [N], double);
 
-void ldu_decomposition(double [N][N], double [N][N], double [N][N], double [N][N]);
+void calculate_residuum(double [N][N], double [N], double [N], double [N]);
 
-bool invert_matrix(double [5][5], double [5][5]);
+double vector_norm(double [N]);
 
-void add_matrices(double [N][N], double [N][N], double [N][N]);
-
-void print_matrix(double [N][N]);
+void print_vector(double [N]);
 
 int main() {
     double M[N][N] = {
@@ -63,7 +61,7 @@ void jacobi(double M[N][N], double b[N], double x0[N]) {
     for (int i = 0; i < N; i++) x[i] = x0[i];
 
     std::cout << "---------------------- Jacobi ---------------------" << std::endl;
-    std::printf("%-5s%-17s%-17s\n", "i", "Estymator", "Residuum");
+    std::printf("%-4s%-36s%-24s%-17s\n", "i", "Wektor x", "Estymator", "Residuum");
 
     for (int iteration = 1; iteration < N_MAX; iteration++) {
         for (int i = 0; i < N; i++) {
@@ -72,43 +70,102 @@ void jacobi(double M[N][N], double b[N], double x0[N]) {
                 if (j != i) sum += M[i][j] * x[j];
             u[i] = (b[i] - sum) / M[i][i];
         }
+
+        double estimator[N], residuum[N];
+        for (int i = 0; i < N; i++) estimator[i] = u[i] - x[i];
+        calculate_residuum(M, b, x, residuum);
+        const double estimator_norm = vector_norm(estimator);
+        const double residuum_norm = vector_norm(residuum);
+        if (estimator_norm < TOLX && residuum_norm< TOLF) break;
+
+        std::cout << iteration << ". ";
+        print_vector(x);
+        std::printf("%2.10f\t\t%2.10f", estimator_norm, residuum_norm);
+        std::cout << std::endl;
+
         for (int i = 0; i < N; i++) x[i] = u[i];
     }
-
-    for (int i = 0; i < N; i++) std::cout << x[i] << " ";
-    std::cout << std::endl;
 }
 
-void gauss_seidel(double M[5][5], double b[5], double x0[5]) {
-    double x[N];
+void gauss_seidel(double M[N][N], double b[N], double x0[N]) {
+    double x[N], x_old[N];
     for (int i = 0; i < N; i++) x[i] = x0[i];
 
+    std::cout << "------------------- Gauss-Seidel ------------------" << std::endl;
+    std::printf("%-4s%-36s%-24s%-17s\n", "i", "Wektor x", "Estymator", "Residuum");
+
     for (int iteration = 1; iteration < N_MAX; iteration++) {
+        for (int i = 0; i < N; i++) x_old[i] = x[i];
+
         for (int i = 0; i < N; i++) {
             double sum = 0.0;
             for (int j = 0; j < N; j++)
                 if (j != i) sum += M[i][j] * x[j];
             x[i] = (b[i] - sum) / M[i][i];
         }
-    }
 
-    for (int i = 0; i < N; i++) std::cout << x[i] << " ";
-    std::cout << std::endl;
+        double estimator[N], residuum[N];
+        for (int i = 0; i < N; i++) estimator[i] = x[i] - x_old[i];
+        calculate_residuum(M, b, x, residuum);
+        const double estimator_norm = vector_norm(estimator);
+        const double residuum_norm = vector_norm(residuum);
+        if (estimator_norm < TOLX && residuum_norm< TOLF) break;
+
+        std::cout << iteration << ". ";
+        print_vector(x);
+        std::printf("%2.10f\t\t%2.10f", estimator_norm, residuum_norm);
+        std::cout << std::endl;
+    }
 }
 
-void sor(double M[5][5], double b[5], double x0[5], const double w) {
-    double x[N];
+void sor(double M[N][N], double b[N], double x0[N], const double w) {
+    double x[N], x_old[N];
     for (int i = 0; i < N; i++) x[i] = x0[i];
 
+    std::cout << "--------------------- SOR ------------------------" << std::endl;
+    std::printf("%-4s%-36s%-24s%-17s\n", "i", "Wektor x", "Estymator", "Residuum");
+
     for (int iteration = 1; iteration < N_MAX; iteration++) {
+        for (int i = 0; i < N; i++) x_old[i] = x[i];
+
         for (int i = 0; i < N; i++) {
             double sum = 0.0;
             for (int j = 0; j < N; j++)
                 if (j != i) sum += M[i][j] * x[j];
-            x[i] = (1.0 - w) * x[i] + (w / M[i][i]) * (b[i] - sum);
+            x[i] = (1.0 - w) * x[i] + w * (b[i] - sum) / M[i][i];
         }
-    }
 
-    for (int i = 0; i < N; i++) std::cout << x[i] << " ";
-    std::cout << std::endl;
+        double estimator[N], residuum[N];
+        for (int i = 0; i < N; i++) estimator[i] = x[i] - x_old[i];
+        calculate_residuum(M, b, x, residuum);
+        const double estimator_norm = vector_norm(estimator);
+        const double residuum_norm = vector_norm(residuum);
+        if (estimator_norm < TOLX && residuum_norm< TOLF) break;
+
+        std::cout << iteration << ". ";
+        print_vector(x);
+        std::printf("%2.10f\t\t%2.10f", estimator_norm, residuum_norm);
+        std::cout << std::endl;
+    }
+}
+
+void calculate_residuum(double M[N][N], double b[N], double x[N], double residuum[N]) {
+    for (int i = 0; i < N; ++i) {
+        double Mx_i = 0.0;
+        for (int j = 0; j < N; ++j) {
+            Mx_i += M[i][j] * x[j];
+        }
+        residuum[i] = b[i] - Mx_i;
+    }
+}
+
+double vector_norm(double x[N]) {
+    double sum = 0.0;
+    for (int i = 0; i < N; i++)
+        sum += x[i] * x[i];
+    return std::sqrt(sum);
+}
+
+void print_vector(double x[5]) {
+    std::printf("%5.2f %5.2f %5.2f %5.2f %5.2f\t", x[0], x[1], x[2], x[3], x[4]);
 }
