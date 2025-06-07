2962e91b658e047b9523989447d34048d4bbd09f
smj-03 <chwastekszymon@gmail.com>
2025-06-07 17:51:36 +0200

    feat: lu laasonen method

--- Lab11.cpp
+++ Lab11.cpp
@@ -2,8 +2,8 @@
 #include <cmath>
 
 #define T_MAX 1
-#define A 0.0L
-#define B 1.0L
+#define X_A 0.0L
+#define X_B 1.0L
 #define D 1.0L
 
 /**
@@ -12,7 +12,7 @@
 #define DT_EXPLICIT 0.001L
 #define DX_EXPLICIT 0.05L
 #define T_NUM_EXPLICIT static_cast<int>(1 / DT_EXPLICIT) + 1
-#define X_NUM_EXPLICIT static_cast<int>((B - A) / DX_EXPLICIT) + 1
+#define X_NUM_EXPLICIT static_cast<int>((X_B - X_A) / DX_EXPLICIT) + 1
 
 /**
  * PARAMETRY DLA METODY LAASONEN
@@ -20,7 +20,7 @@
 #define DT_IMPLICIT 0.0025L
 #define DX_IMPLICIT 0.05L
 #define T_NUM_IMPLICIT static_cast<int>(1 / DT_IMPLICIT) + 1
-#define X_NUM_IMPLICIT static_cast<int>((B - A) / DX_IMPLICIT) + 1
+#define X_NUM_IMPLICIT static_cast<int>((X_B - X_A) / DX_IMPLICIT) + 1
 
 #define X_NUM 1000
 
@@ -28,18 +28,24 @@ long double analytical_solution(const long double x, const long double t) {
     return (1.0L - std::expl(-std::numbers::pi * std::numbers::pi * D * t)) * std::sinl(std::numbers::pi * x);
 }
 
-void explicit_method(long double (&u)[T_NUM_EXPLICIT][X_NUM_EXPLICIT]);
-
-void implicit_method(long double (&u)[T_NUM_IMPLICIT][X_NUM_IMPLICIT]);
+void explicit_method(long double u[T_NUM_EXPLICIT][X_NUM_EXPLICIT]);
 
 void thomas_algorithm(long double *d, const long double *l, const long double *u, long double *b, long double *x,
                       int n);
+
+void lu_decomposition(long double M[X_NUM_IMPLICIT][X_NUM_IMPLICIT], int n);
+
+void solve_lu_equation(long double M[X_NUM_IMPLICIT][X_NUM_IMPLICIT], long double *b, int n);
+
+void implicit_method_thomas(long double u[T_NUM_IMPLICIT][X_NUM_IMPLICIT]);
+
+void implicit_method_lu(long double u[T_NUM_IMPLICIT][X_NUM_IMPLICIT]);
 
 void linear_space(long double a, long double b, int n, long double *output);
 
 int main() {
     long double space[X_NUM];
-    linear_space(A, B, X_NUM, space);
+    linear_space(X_A, X_B, X_NUM, space);
 
     FILE *file1 = fopen("../data/Lab11/wykres_analityczny", "w");
     constexpr long double t_values[] = {0.0L, 0.1L, 0.2L, 0.3L, 0.4L, 0.5L};
@@ -57,7 +63,7 @@ int main() {
 
     FILE *file2 = fopen("../data/Lab11/wykres_numeryczny", "w");
     for (int i = 0; i < X_NUM_EXPLICIT; i++) {
-        const long double xi = A + i * DX_EXPLICIT;
+        const long double xi = X_A + i * DX_EXPLICIT;
         fprintf(file2, "%.12LE", xi);
         for (int j = 0; j < t_count; j++) {
             const int t_index = static_cast<int>(t_values[j] / DT_EXPLICIT);
@@ -67,35 +73,50 @@ int main() {
     }
     fclose(file2);
 
-    long double u_implicit[T_NUM_IMPLICIT][X_NUM_IMPLICIT] = {0.0};
-    implicit_method(u_implicit);
+    long double u_implicit_thomas[T_NUM_IMPLICIT][X_NUM_IMPLICIT] = {0.0};
+    implicit_method_thomas(u_implicit_thomas);
 
-    FILE *file3 = fopen("../data/Lab11/wykres_numeryczny_laasonen", "w");
+    FILE *file3 = fopen("../data/Lab11/wykres_numeryczny_laasonen_thomas", "w");
     for (int i = 0; i < X_NUM_IMPLICIT; i++) {
-        const long double xi = A + i * DX_IMPLICIT;
+        const long double xi = X_A + i * DX_IMPLICIT;
         fprintf(file3, "%.12LE", xi);
         for (int j = 0; j < t_count; j++) {
             const int t_index = static_cast<int>(t_values[j] / DT_IMPLICIT);
-            fprintf(file3, "\t%.12LE", u_implicit[t_index][i]);
+            fprintf(file3, "\t%.12LE", u_implicit_thomas[t_index][i]);
         }
         fprintf(file3, "\n");
     }
     fclose(file3);
 
+    long double u_implicit_lu[T_NUM_IMPLICIT][X_NUM_IMPLICIT] = {0.0};
+    implicit_method_lu(u_implicit_lu);
+
+    FILE *file4 = fopen("../data/Lab11/wykres_numeryczny_laasonen_lu", "w");
+    for (int i = 0; i < X_NUM_IMPLICIT; i++) {
+        const long double xi = X_A + i * DX_IMPLICIT;
+        fprintf(file4, "%.12LE", xi);
+        for (int j = 0; j < t_count; j++) {
+            const int t_index = static_cast<int>(t_values[j] / DT_IMPLICIT);
+            fprintf(file4, "\t%.12LE", u_implicit_lu[t_index][i]);
+        }
+        fprintf(file4, "\n");
+    }
+    fclose(file4);
+
     return 0;
 }
 
-void explicit_method(long double (&u)[T_NUM_EXPLICIT][X_NUM_EXPLICIT]) {
+void explicit_method(long double u[T_NUM_EXPLICIT][X_NUM_EXPLICIT]) {
     constexpr long double lambda = D * DT_EXPLICIT / (DX_EXPLICIT * DX_EXPLICIT);
     for (int k = 0; k < T_NUM_EXPLICIT - 1; k++)
         for (int i = 1; i < X_NUM_EXPLICIT - 1; i++) {
-            const long double x_i = A + i * DX_EXPLICIT;
+            const long double x_i = X_A + i * DX_EXPLICIT;
             u[k + 1][i] = lambda * u[k][i - 1] + (1.0L - 2.0L * lambda) * u[k][i] + lambda * u[k][i + 1] +
                           DT_EXPLICIT * D * std::numbers::pi * std::numbers::pi * std::sinl(std::numbers::pi * x_i);
         }
 }
 
-void implicit_method(long double (&u)[T_NUM_IMPLICIT][X_NUM_IMPLICIT]) {
+void implicit_method_thomas(long double u[T_NUM_IMPLICIT][X_NUM_IMPLICIT]) {
     constexpr long double lambda = D * DT_IMPLICIT / (DX_IMPLICIT * DX_IMPLICIT);
     constexpr int N = X_NUM_IMPLICIT;
     constexpr long double diagonal_value = -(1.0L + 2.0L * lambda);
@@ -116,7 +137,7 @@ void implicit_method(long double (&u)[T_NUM_IMPLICIT][X_NUM_IMPLICIT]) {
         b[0] = 0.0L;
         b[N - 1] = 0.0L;
         for (int i = 1; i < N - 1; ++i) {
-            const long double x_i = A + i * DX_IMPLICIT;
+            const long double x_i = X_A + i * DX_IMPLICIT;
             const long double source_term = D * DT_IMPLICIT * std::numbers::pi * std::numbers::pi * sinl(
                                                 std::numbers::pi * x_i);
             b[i] = -u[t - 1][i] - source_term;
@@ -130,7 +151,7 @@ void implicit_method(long double (&u)[T_NUM_IMPLICIT][X_NUM_IMPLICIT]) {
 void thomas_algorithm(long double *d, const long double *l, const long double *u, long double *b, long double *x,
                       const int n) {
     for (int i = 1; i < n; i++) {
-        const double m = l[i - 1] / d[i - 1];
+        const long double m = l[i - 1] / d[i - 1];
         d[i] = d[i] - m * u[i - 1];
         b[i] = b[i] - m * b[i - 1];
     }
@@ -138,6 +159,68 @@ void thomas_algorithm(long double *d, const long double *l, const long double *u
     x[n - 1] = b[n - 1] / d[n - 1];
     for (int i = n - 2; i >= 0; i--) {
         x[i] = (b[i] - u[i] * x[i + 1]) / d[i];
+    }
+}
+
+void implicit_method_lu(long double u[T_NUM_IMPLICIT][X_NUM_IMPLICIT]) {
+    constexpr long double lambda = D * DT_IMPLICIT / (DX_IMPLICIT * DX_IMPLICIT);
+    constexpr int N = X_NUM_IMPLICIT;
+    constexpr long double diagonal_value = -(1.0L + 2.0L * lambda);
+
+    long double A[N][N] = {0.0}, b[N] = {0.0};
+
+    for (int i = 0; i < N; ++i) {
+        A[i][i] = diagonal_value;
+        if (i > 0) A[i][i - 1] = lambda;
+        if (i < N - 1) A[i][i + 1] = lambda;
+    }
+
+    A[0][0] = 1.0L;
+    A[0][1] = 0.0L;
+    A[N - 1][N - 1] = 1.0L;
+    A[N - 1][N - 2] = 0.0L;
+
+    lu_decomposition(A, N);
+
+    for (int t = 1; t <= T_NUM_IMPLICIT; t++) {
+        for (int i = 1; i < N - 1; i++) {
+            const long double x_i = X_A + i * DX_IMPLICIT;
+            const long double source_term = D * DT_IMPLICIT * std::numbers::pi * std::numbers::pi * sinl(
+                                                std::numbers::pi * x_i);
+            b[i] = -u[t - 1][i] - source_term;
+        }
+        b[0] = 0.0L;
+        b[N - 1] = 0.0L;
+
+        solve_lu_equation(A, b, N);
+        for (int i = 0; i < N; i++) u[t][i] = b[i];
+    }
+}
+
+void lu_decomposition(long double M[X_NUM_IMPLICIT][X_NUM_IMPLICIT], const int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int k = i + 1; k < n; k++) {
+            const long double quotient = M[k][i] / M[i][i];
+            M[k][i] = quotient;
+            for (int j = i + 1; j < n; j++)
+                M[k][j] -= M[i][j] * quotient;
+        }
+    }
+}
+
+void solve_lu_equation(long double M[X_NUM_IMPLICIT][X_NUM_IMPLICIT], long double *b, const int n) {
+    long double y[n];
+
+    for (int i = 0; i < n; i++) {
+        long double sum = 0.0;
+        for (int j = 0; j < i; j++) sum += M[i][j] * y[j];
+        y[i] = b[i] - sum;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        long double sum = 0.0;
+        for (int j = i + 1; j < n; j++) sum += M[i][j] * b[j];
+        b[i] = (y[i] - sum) / M[i][i];
     }
 }
 
